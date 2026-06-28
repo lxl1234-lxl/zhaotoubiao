c6d346e8af15f040f9d69e92cc5a20b5dbd0c744
lxl1234-lxl <2702788355@qq.com>
2026-06-28 23:48:28 +0800

    更新部署路径为 zhaotoubiao

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={encodeURI('/招标投标网站')}>
+    <BrowserRouter basename="/zhaotoubiao">
       <App />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/招标投标网站/',
+  base: '/zhaotoubiao/',
   plugins: [react(), tailwindcss()],
 })
